2f1f1d1a6d367a01d280d174635927056f5b16e1
richard <xdlisjtu@gmail.com>
2017-04-24 00:11:21 -0700

    CPU overhead with Q1 and Q2

---
+++ include/utilities.h
@@ -0,0 +1,18 @@
+//
+// Created by Xingdong Li on 4/23/17.
+//
+
+#ifndef CSE221_UTILITIES_H
+#define CSE221_UTILITIES_H
+
+void procedure0() {}
+void procedure1(int p1) {}
+void procedure2(int p1, int p2) {}
+void procedure3(int p1, int p2, int p3) {}
+void procedure4(int p1, int p2, int p3, int p4) {}
+void procedure5(int p1, int p2, int p3, int p4, int p5) {}
+void procedure6(int p1, int p2, int p3, int p4, int p5, int p6) {}
+void procedure7(int p1, int p2, int p3, int p4, int p5, int p6, int p7) {}
+
+
+#endif //CSE221_UTILITIES_H

7606488b6f5eb211b5adeb4eab7d5538ef69cea3
Zhuodi Liu <zhuodiliu@gmail.com>
2017-05-13 18:14:39 -0700

    Q2 & Q3

--- src/MEM.cpp
+++ src/MEM.cpp
@@ -5,6 +5,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "rdtsc.h"
+#include "utilities.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <iostream>
+#include <cstring>
+#include <ctime>
+using namespace std;
+#define LOOP_TIMES 10
 #define LARGE_LOOP_TIMES 100
 
 double getMemoryAccessTime(long size, long stride) {
@@ -26,6 +37,87 @@ double getMemoryAccessTime(long size, long stride) {
     return sum / LARGE_LOOP_TIMES;
 }
 
+void getMemoryBindwidth() {
+    long arrayLength = 1 << 24;//16mb*64=1024mb
+    int* intArray = (int*)malloc(arrayLength*sizeof(int));
+    memset(intArray, -1, arrayLength*sizeof(int));
+    long unrollLength = arrayLength / 8;
+    uint64_t start, end;
+    int tmp;
+    int blockNumber = 8;
+    long blockLength = arrayLength / blockNumber;
+    for (int l=0; l<LOOP_TIMES; ++l) {
+        start = rdtsc_start();
+        for (int i=0; i<32; ++i) {
+            for (long j=0;j<blockLength;j+=32) {//avoid cache
+                tmp = intArray[i+j];
+                tmp = intArray[i+j+blockLength];
+                tmp = intArray[i+j+blockLength*2];
+                tmp = intArray[i+j+blockLength*3];
+                tmp = intArray[i+j+blockLength*4];
+                tmp = intArray[i+j+blockLength*5];
+                tmp = intArray[i+j+blockLength*6];
+                tmp = intArray[i+j+blockLength*7];
+            }
+        }
+        end = rdtsc_end();
+        printf("RAM read overhead = %ld\n", end - start);
+    }
+    for (int l=0; l<LOOP_TIMES; ++l) {
+        start = rdtsc_start();
+        for (int i=0; i<32; ++i) {
+            for (long j=0;j<blockLength;j+=32) {//avoid cache
+                intArray[i+j] = 1;
+                intArray[i+j+blockLength] = 1;
+                intArray[i+j+blockLength*2] = 1;
+                intArray[i+j+blockLength*3] = 1;
+                intArray[i+j+blockLength*4] = 1;
+                intArray[i+j+blockLength*5] = 1;
+                intArray[i+j+blockLength*6] = 1;
+                intArray[i+j+blockLength*7] = 1;
+            }
+        }
+        end = rdtsc_end();
+        printf("RAM write overhead = %ld\n", end - start);
+    }
+    free(intArray);
+}
+
+void createFile() {
+    srand(time(NULL));
+    long arraySize = 1 << 26;//64mb
+    char *writeChar = (char*)malloc(arraySize*sizeof(char));
+    memset(writeChar, 'A', arraySize*sizeof(char));
+    long f;
+    f = open("testFile", O_WRONLY | O_CREAT);
+    write(f, writeChar, arraySize);
+    close(f);
+}
+
+void getPageFaultTime() {
+    long arraySize = 1 << 26;//64mb
+    char* data = NULL;
+    long f;
+    uint64_t start, end;
+    f = open("testFile", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+    if (f == -1) {
+        printf("File Open Failed\n");
+        return;
+    }
+    char* m = (char*)mmap(NULL, arraySize, PROT_READ | PROT_WRITE, MAP_SHARED, f, 0);
+    if (m == MAP_FAILED) {
+        printf("MMAP Failed\n");
+        return;
+    }
+    close(f);
+    start = rdtsc_start();
+    for (int i=0; i<8192; ++i) {
+        m[i*8192] = 'A';
+    }
+    end = rdtsc_end();
+    munmap(m, arraySize);
+    printf("Page Fault Overhead = %ld\n", end - start);
+}
 
 int main() {
     long stride[6] = {4, 16, 64, 256, 1028, 4096};
@@ -42,6 +134,10 @@ int main() {
         }
         printf("------------------------------\n");
     }
-
-
+    getMemoryBindwidth();
+    createFile();
+    for (int i=0; i<LOOP_TIMES; ++i) {
+        getPageFaultTime();
+    }
+    return 0;
 }
